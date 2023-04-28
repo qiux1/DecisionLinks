a2b7f1647eba5e673c854d09f7d5349de105d2cc
Xinyu Qiu <qiux1@seattleu.edu>
2023-04-28 11:20:52 -0700

    Created first method and comments for FindFistNonRepeatingCharacter. Changed in Program.cs

--- DecisionLinks/Program.cs
+++ DecisionLinks/Program.cs
@@ -1,1 +1,20 @@
-﻿    
+﻿class Program
+{
+    /// <summary>
+    /// A method to find the first non-repeating character in a provided string
+    /// return null if the input does not have a non-repeating character
+    /// else return the first non-repeating character
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static char? FindFistNonRepeatingCharacter(string input)
+    {
+        //if there is no non-repeating character from input string
+        return null;
+    }
+
+    public static void Main(string[] args)
+    {
+        
+    }
+}
