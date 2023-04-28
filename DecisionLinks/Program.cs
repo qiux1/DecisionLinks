93c8d8fb4cbe11c2f89d9110a2d84a710d61c343
Xinyu Qiu <qiux1@seattleu.edu>
2023-04-28 11:25:43 -0700

    use foreach to iterate the input string to add each character and occurence to the map dictionary

--- DecisionLinks/Program.cs
+++ DecisionLinks/Program.cs
@@ -9,6 +9,25 @@
     /// <returns></returns>
     public static char? FindFistNonRepeatingCharacter(string input)
     {
+        //use dictonary to store the frequence of each character from input string
+        Dictionary<char, int> map = new Dictionary<char, int>();
+
+        //iterate through the input string using foreach 
+        foreach(char c in input)
+        {
+            //if the map already contains the character
+            if (map.ContainsKey(c))
+            {
+                //increment the value by 1
+                map[c]++;
+            }
+            else
+            {
+                //if the map does not contain the character,
+                //add the <c, 1> into the map dictionary
+                map.Add(c, 1);
+            }
+        }
         //if there is no non-repeating character from input string
         return null;
     }
